10fe6f6a8a9319cdf195116db8fbdebefaaff546
Nitesh Dayma <niteshsharma2698@gmail.com>
2025-11-16 23:52:37 +0530

    Time: 1482 ms (27.22%), Space: 18.1 MB (38.61%) - LeetHub

--- 3479-count-the-number-of-substrings-with-dominant-ones/3479-count-the-number-of-substrings-with-dominant-ones.cpp
+++ 3479-count-the-number-of-substrings-with-dominant-ones/3479-count-the-number-of-substrings-with-dominant-ones.cpp
@@ -2,29 +2,30 @@ class Solution {
 public:
     int numberOfSubstrings(string s) {
         int n = s.length();
-        vector<int>countone(n,0);
-        countone[0] = (s[0]=='1'?1:0);
+        vector<int>countone(n);
+        countone[0] = (s[0]=='1' ? 1 : 0);
         for(int i=1;i<n;i++){
-            countone[i] = countone[i-1] + ((s[i]=='1') ? 1 : 0);
+            countone[i] = countone[i-1] + (s[i]=='1' ? 1 : 0);
         }
         int result =0;
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-                int count_one = countone[j] - (i-1>=0 ? countone[i-1] : 0);
+                int count_one = countone[j] - (i-1 >=0 ? countone[i-1] : 0);
                 int count_zero = (j-i+1) - count_one;
+
                 if((count_zero*count_zero) > count_one){
-                    int jump = count_zero*count_zero - count_one;
+                    int jump = (count_zero*count_zero) - count_one;
                     j+=jump-1;
-                }
-                else if((count_zero*count_zero)==count_one){
-                     result+=1;
+                } 
+                else if((count_zero*count_zero) == count_one){
+                     result++;
                 }
                 else{
-                    result+=1;
+                    result++;
                     int k = sqrt(count_one) - count_zero;
-                    int next = j +k;
-                    if(next>=n) {
-                        result += (n-j-1);
+                    int next = j+k;
+                    if(next>=n){
+                        result+=n-j-1;
                         break;
                     }
                     else{
