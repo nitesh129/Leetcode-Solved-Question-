c6978ae999042b848539f7204064c73f319e185a
Nitesh Dayma <niteshsharma2698@gmail.com>
2025-11-27 09:53:14 +0530

    Time: 1174 ms (61.54%), Space: 0B (100%) - LeetHub

---
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select 
+   Product.product_name,
+   Sales.year,
+   Sales.price
+   from Sales 
+   join Product 
+   ON Sales.product_id = Product.product_id;
