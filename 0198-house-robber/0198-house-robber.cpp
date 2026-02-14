bc52546717924eeaf14c9b63e59238810384e056
Nitesh Dayma <niteshsharma2698@gmail.com>
2026-02-14 22:58:22 +0530

    Time: 0 ms (100%), Space: 10.1 MB (78.16%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-   int t[101];
-   int solve(vector<int>&nums , int i){
-      if(i>=nums.size()) return 0;
-      if(t[i]!=-1) return t[i];
-      return t[i] = max((nums[i] + solve(nums,i+2)) , (solve(nums,i+1)));
-   }
+    int t[101];
+    int solve(vector<int>&nums , int n , int i){
+        if(i>=n) return 0;
+        if(t[i]!=-1) return t[i];
+        return t[i] = max(nums[i] + solve(nums,n,i+2) , solve(nums,n,i+1));
+        
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
         memset(t,-1,sizeof(t));
-        return solve(nums,0);
+        return solve(nums,n,0);
     }
 };
