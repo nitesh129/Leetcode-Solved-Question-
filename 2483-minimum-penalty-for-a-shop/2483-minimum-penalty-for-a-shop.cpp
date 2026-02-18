675c47b84276bd9d57eaa753ce91006f7fbd46fa
Nitesh Dayma <niteshsharma2698@gmail.com>
2026-02-18 10:16:10 +0530

    Time: 9 ms (31.23%), Space: 17.6 MB (28.07%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
-        int n = customers.size();
-        
-        // prefixY[i] = number of 'Y' in customers[0..i-1]
-        vector<int> prefixY(n + 1, 0);
-
-        for (int i = 0; i < n; i++) {
-            prefixY[i + 1] = prefixY[i] + (customers[i] == 'Y');
-        }
-
-        int totalY = prefixY[n];
-        int bestHour = 0;
-        int minPenalty = INT_MAX;
-
-        for (int j = 0; j <= n; j++) {
-            int openHours = j;
-            int openY = prefixY[j];
-            int openN = openHours - openY;           // hours we were open but no customers
-            
-            int closedY = totalY - prefixY[j];       // hours we were closed but customers arrived
-            
-            int penalty = openN + closedY;
-
-            if (penalty < minPenalty) {
-                minPenalty = penalty;
-                bestHour = j;
-            }
-        }
-
-        return bestHour;
+        int n = customers.length();
+       vector<int>suffix(n+1,0);
+       suffix[n] =0;
+       for(int i=n-1;i>=0;i--){
+          suffix[i] = suffix[i+1] + (customers[i] == 'Y' ? 1 : 0);
+       }
+       int N_count =0;
+       int min_ans = INT_MAX;
+       int min_penalty = INT_MAX;
+       int ans =0;
+       for(int i=0;i<=n;i++){
+         int penalty = suffix[i] + N_count;
+         if(penalty < min_penalty){
+            min_penalty = penalty;
+            ans =i;
+         }
+           
+          if(customers[i]=='N') N_count++;
+       }
+        return ans;
     }
 };
