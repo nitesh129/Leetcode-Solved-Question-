a9404321922df361621836afd75fcdee871c09ec
Nitesh Dayma <niteshsharma2698@gmail.com>
2026-02-18 12:14:36 +0530

    Time: 283 ms (31.57%), Space: 201.7 MB (32.78%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int t[2501][2501];
-    int solve(vector<int>&nums , int n , int indx , int prev){
-        if(indx >= n){
-           return 0;
-        }
-        if(t[indx][prev+1]!=-1) return t[indx][prev+1];
-        int take =0;
-        if(prev==-1 || nums[prev] < nums[indx]){
-             take = 1+solve(nums,n,indx+1,indx);
-        }
-        int not_take = solve(nums,n,indx+1,prev);
-        return t[indx][prev+1] = max(take,not_take);
-        
-    }
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        memset(t,-1,sizeof(t));
-      return solve(nums,n,0,-1);
-    
+        vector<int>nums2 = nums;
+        sort(begin(nums2),end(nums2));
+        nums2.erase(unique(nums2.begin(), nums2.end()), nums2.end()); // remove unique elements 
+        int m = nums.size();
+        int n = nums2.size();
+        vector<vector<int>>t(m+1,vector<int>(n+1,0));
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(nums[i-1] == nums2[j-1]){
+                    t[i][j] = 1 + t[i-1][j-1];
+                }
+                else{
+                    t[i][j] = max(t[i-1][j] , t[i][j-1]);
+                }
+            }
+        }
+        return t[m][n];
+
     }
 };
